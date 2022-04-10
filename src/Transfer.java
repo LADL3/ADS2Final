6d1632aff3e6be1998ef0440da442e7c8ef11087
LADL3 <79784026+LADL3@users.noreply.github.com>
2022-04-10 13:35:38 +0100

    added constructor and get functions for Transfer.java

--- src/Transfer.java
+++ src/Transfer.java
@@ -1,2 +1,51 @@
 public class Transfer {
+    private int from_stop_id;
+    private int to_stop_id;
+    private int transfer_type;
+    private int min_transfer_time;
+
+    public Transfer(String in) {
+        String[] input = in.split(",");
+
+        try {
+            this.from_stop_id = Integer.parseInt(input[0]);
+        } catch (Exception e) {
+            this.from_stop_id = -1;
+        }
+
+        try {
+            this.to_stop_id = Integer.parseInt(input[1]);
+        } catch (Exception e) {
+            this.to_stop_id = -1;
+        }
+
+        try {
+            this.transfer_type = Integer.parseInt(input[2]);
+        } catch (Exception e) {
+            this.transfer_type = -1;
+        }
+
+        try {
+            this.min_transfer_time = Integer.parseInt(input[3]);
+        } catch (Exception e) {
+            this.min_transfer_time = -1;
+        }
+
+    }
+
+    public int getFromStopId() {
+        return from_stop_id;
+    }
+
+    public int getToStopId() {
+        return to_stop_id;
+    }
+
+    public int getTransferTime() {
+        return transfer_type;
+    }
+
+    public int getTransferType() {
+        return min_transfer_time;
+    }
 }
